a55df33cf81deef9a28bd5a693edd95cbbfa35f2
Lance Hannestad <lhannest@sfu.ca>
2018-02-16 15:58:53 -0800

    Implemented exactmatches - Unfortunately this requires ontobio, so we're not free of this   dependency

--- backend/controller_impl/exactmatches_controller_impl.py
+++ backend/controller_impl/exactmatches_controller_impl.py
@@ -1,8 +1,5 @@
-import connexion
-import six
-
-from swagger_server import util
-
+from controller_impl import utils
+from ontobio.golr.golr_query import GolrSearchQuery
 
 def get_exact_matches_to_concept(conceptId):  # noqa: E501
     """get_exact_matches_to_concept
@@ -14,17 +11,38 @@ def get_exact_matches_to_concept(conceptId):  # noqa: E501
 
     :rtype: List[str]
     """
-    return 'do some magic!'
-
+    return _get_exact_matches(conceptId)
 
 def get_exact_matches_to_concept_list(c):  # noqa: E501
     """get_exact_matches_to_concept_list
 
     Given an input list of [CURIE](https://www.w3.org/TR/curie/) identifiers of known exactly matched concepts [*sensa*-SKOS](http://www.w3.org/2004/02/skos/core#exactMatch), retrieves the list of [CURIE](https://www.w3.org/TR/curie/) identifiers of additional concepts that are deemed by the given knowledge source to be exact matches to one or more of the input concepts **plus** whichever identifiers from the input list which specifically matched these new additional concepts.  If an empty set is returned, the it can be assumed that the given  knowledge source does not know of any new equivalent concepts matching the input set.  # noqa: E501
 
-    :param c: set of [CURIE-encoded](https://www.w3.org/TR/curie/) identifiers of exactly matching concepts, to be used in a search for additional exactly matching concepts [*sensa*-SKOS](http://www.w3.org/2004/02/skos/core#exactMatch). 
+    :param c: set of [CURIE-encoded](https://www.w3.org/TR/curie/) identifiers of exactly matching concepts, to be used in a search for additional exactly matching concepts [*sensa*-SKOS](http://www.w3.org/2004/02/skos/core#exactMatch).
     :type c: List[str]
 
     :rtype: List[str]
     """
-    return 'do some magic!'
+    s = set(c)
+    for conceptId in c:
+        exactmatches = _get_exact_matches(conceptId)
+        s = s.union(exactmatches)
+    return list(s)
+
+def _get_exact_matches(conceptId):
+    results = GolrSearchQuery(
+        term=conceptId,
+        fq={'id' : conceptId},
+        rows=1,
+        hl=False
+    ).exec()
+
+    docs = results['docs']
+
+    exactmatches = []
+    for d in docs:
+        if  utils.get_property(d, 'id') == conceptId:
+            # exactmatches = get_concept_property(d, 'equivalent_curie')
+            matches = utils.get_property(d, 'equivalent_curie', [])
+            exactmatches.extend(matches)
+    return exactmatches
