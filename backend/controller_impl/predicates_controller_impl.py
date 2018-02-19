93f78c2e6a89da992753c253a55c4dc3246435bf
Lance Hannestad <lhannest@sfu.ca>
2018-02-19 10:27:06 -0800

    Implemented summary and predicates endpoints

--- backend/controller_impl/predicates_controller_impl.py
+++ backend/controller_impl/predicates_controller_impl.py
@@ -1,16 +1,34 @@
-import connexion
-import six
+from ontobio.golr.golr_query import GolrAssociationQuery, M
+from swagger_server.models.predicate import Predicate
 
-from swagger_server.models.predicate import Predicate  # noqa: E501
-from swagger_server import util
+def get_predicates():
+	"""get_predicates
 
-
-def get_predicates():  # noqa: E501
-    """get_predicates
-
-    Get a list of predicates used in statements issued by the knowledge source  # noqa: E501
+	Get a list of predicates used in statements issued by the knowledge source  # noqa: E501
 
 
-    :rtype: List[Predicate]
-    """
-    return 'do some magic!'
+	:rtype: List[Predicate]
+	"""
+
+	g = GolrAssociationQuery(
+		q='*:*',
+		facet_fields=[M.RELATION, M.RELATION_LABEL],
+		facet_pivot_fields=[M.RELATION, M.RELATION_LABEL],
+		rows=0
+	)
+
+	results = g.exec()
+	facet_pivot = results['facet_pivot']
+
+	predicates = []
+
+	for p in facet_pivot['relation,relation_label']:
+		identifier = p['value']
+		for pivot in p['pivot']:
+			# not currently used
+			count = pivot['count']
+			name = pivot['value']
+			predicate = Predicate(id=identifier, name=name, definition=None)
+			predicates.append(predicate)
+
+	return predicates
